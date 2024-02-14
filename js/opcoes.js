91e67066da71e520a299ee186f417e9f012f8354
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-02-14 20:57:51 +0000

    alterando arquivos

--- js/opcoes.js
+++ js/opcoes.js
@@ -5,19 +5,19 @@
 function opcoes(){
     op = document.getElementsByClassName("op");
     //opA = op.getAttibute("id");
-    opA = document.getElementById("op-a");
-
+    opA = document.getElementById("op-a"); 
+    
     //var item = document.getElementById(id); //Capturando o campo de índice da tarefa
     //var classe = item.getAttribute('class'); //Capturando o atributo "class", que é o nome da classe
     //opB = document.getElementById("op-b").className;
     //opC = document.getElementById("op-c").className;
-    if (op == opA.id ){
-        console.log(op + "teste A" + opA);
+    if (op.id == "op-a" ){
+        console.log(op.id + "teste A" + opA);
         document.getElementById("text").innerHTML = op;
         document.getElementById("text2").innerHTML = opA;
 
     }else{
-        console.log("teste B")
+        console.log("teste B");
         console.log(op.id + "teste B" + opA.id);
     }
 

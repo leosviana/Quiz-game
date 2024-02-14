cdcff36b503aeb86c7e4d28c195e8090c3ca37e5
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-02-14 17:22:53 +0000

    alterando arquivos

---
+++ js/opcoes.js
@@ -0,0 +1,20 @@
+//1 - Quando selecionar uma opção, dar uma descrição de “marcado” 
+//2 - Ajustar para a cor de verde
+//3 - Marcar as outras opções com a cor azul
+
+function opcoes(){
+    op = document.getElementsByClassName("op");
+    opA = document.getElementById("op-a");
+    //opB = document.getElementById("op-b").className;
+    //opC = document.getElementById("op-c").className;
+    if (op == opA ){        
+        console.log("opcao A");
+        //console.log(op[0]);
+        //console.log(op[1]);
+        //console.log(op[2]);
+    }
+    
+    
+
+
+}

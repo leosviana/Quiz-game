e058eead6692a7d6fce75b2c3f6b38efcb5172bb
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-02-16 20:59:29 +0000

    alterando arquivos

--- js/opcoes.js
+++ js/opcoes.js
@@ -2,31 +2,50 @@
 //2 - Ajustar para a cor de verde
 //3 - Marcar as outras opções com a cor azul
 
-function opcoes(){
-    op = document.getElementsByClassName("op").class;
-    //opA = op.getAttibute("id");
-    opA = document.getElementById("op-a"); 
-    //console.log("tst" + OpA);
-    console.log(op + opA.id);
-    /*switch(){
-        case opA:
-            console.log(OpA.id);
+
+
+document.querySelectorAll('.op').forEach(function(opcoes){
+    opcoes.addEventListener('onclick', function(){
+        var idBotao = this.id;
+
+        switch(idBotao){
+            case 'op-a':
+                console("Teste A");
+            break;
+            case 'op-b':
+                console("Teste B");
+            break;
+            default:
+                console("outro");
+        }
+    })
+})
+
+
+
+/*function opcoes(){
+    idBotao = this.id;
+    op = document.querySelector(".op");
+    //opA = document.getElementById("op-a");     
+    switch(idBotao){
+        case 'op-a':
+            console.log("Opcao A");
         break;
         default:
             console.log("outro");
-    }*/
+    }
+
+
+
     //var item = document.getElementById(id); //Capturando o campo de índice da tarefa
     //var classe = item.getAttribute('class'); //Capturando o atributo "class", que é o nome da classe
-    //opB = document.getElementById("op-b").className;
-    //opC = document.getElementById("op-c").className;
-    /*if (op.id == "op-a"){
-        console.log(op.id + " teste A " + opA);
-        document.getElementById("text").innerHTML = op;
-        document.getElementById("text2").innerHTML = opA;
-
+   
+    /*if (op.classList && opA){
+        console.log(" teste A " + opA.id);
+    }else if (op.classList && opB){
+        console.log(" teste B" + opB.id);
     }else{
-        console.log("teste B");
-        console.log(op.id + " teste B " + opA.id);
+        console.log(" outro ");
     }*/
 
-}
+ //}

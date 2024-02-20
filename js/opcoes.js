16582809bee76f6c67f2060208c1d0142cfb0e5f
Leonardo S. Viana <94654528+leosviana@users.noreply.github.com>
2024-02-20 19:35:48 +0000

    alterando arquivos

--- js/opcoes.js
+++ js/opcoes.js
@@ -4,37 +4,38 @@
 
 
 function opcoes() {
-    var opA = document.getElementById("op-a");
-    var opB = document.getElementById("op-b");
-    var opC = document.getElementById("op-c");    
+    var opA = document.getElementById("op-a"); //Opção A
+    var opB = document.getElementById("op-b"); //Opção B
+    var opC = document.getElementById("op-c"); //Opção C
+    var status = "desmarcado"; 
 
     // Adiciona um evento de clique a todos os elementos com a classe "op"
-    document.querySelectorAll('.op').forEach(function(botao) {
-      botao.addEventListener('click', function() {
-        // Obtém o ID do botão clicado
-        var idDoBotao = this.id;
-  
-        // Executa ação com base no ID do botão
-        switch (idDoBotao) {
-          case 'op-a':
-            console.log("Teste a" + opA.id);
-            var status = "marcado";
+    document.querySelectorAll('.op').forEach(function(botao){
+      botao.addEventListener('click', function() {        
+        var idDoBotao = this.id; // Obtém o ID do botão clicado
+          
+        switch (idDoBotao) { // Executa ação com base no ID do botão
+          case 'op-a': //Caso clique no botão da opção A            
             opA.style.backgroundColor = "green";
             opB.style.backgroundColor = "blue";
             opC.style.backgroundColor = "blue";
+            var status = "marcado";
+            console.log("Teste a -> " + status);            
             
             break;
-          case 'op-b':
-            console.log("Teste b");
+          case 'op-b': //Caso clique no botão da opção B
             opA.style.backgroundColor = "blue";
             opB.style.backgroundColor = "green";
             opC.style.backgroundColor = "blue";
+            var status = "marcado";
+            console.log("Teste b -> " + status);
             break;
-          case 'op-c':
-            console.log("Teste c");
+          case 'op-c': //Caso clique no botão da opção C
             opA.style.backgroundColor = "blue";
             opB.style.backgroundColor = "blue";
             opC.style.backgroundColor = "green";
+            var status = "marcado";
+            console.log("Teste c -> " + status);
             break;
           default:
             console.log("Botão não reconhecido");

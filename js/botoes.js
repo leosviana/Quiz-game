cd351be41fd8e4c5574ce651c326d097652878a6
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-02-21 07:26:24 -0300

    Update botoes.js

--- js/botoes.js
+++ js/botoes.js
@@ -4,62 +4,59 @@
 //2 - Ajustar para a cor de verde
 //3 - Marcar as outras opções com a cor azul
 
-var statusOpA = "desmarcado";
-var statusOpB = "desmarcado";
-var statusOpC = "desmarcado";
+var statusOpA = "desmarcado"; //Iniciar status das opções como desmarcada
+var statusOpB = "desmarcado"; // --
+var statusOpC = "desmarcado"; // --
+var opA = document.getElementById("op-a"); //Opção A
+var opB = document.getElementById("op-b"); //Opção B
+var opC = document.getElementById("op-c"); //Opção C
+var idQuiz = document.getElementById("id-quiz").innerHTML = "QUIZ " + 1; //Número do quiz
+var idPergunta = document.getElementById("id-pergunta").innerHTML = 1; //Número da pergunta
 
-//function btOpcoes(){ //Não foi necessário incluir função do botão 
-    var opA = document.getElementById("op-a"); //Opção A
-    var opB = document.getElementById("op-b"); //Opção B
-    var opC = document.getElementById("op-c"); //Opção C
-    var idQuiz = document.getElementById("id-quiz").innerHTML = "QUIZ " + 1; //Número do quiz
-    var idPergunta = document.getElementById("id-pergunta").innerHTML = 1; //Número da pergunta
-
-    // Adiciona um evento de clique a todos os elementos com a classe "op"
-    document.querySelectorAll('.op').forEach(function(botao){
-      botao.addEventListener('click', function(){        
-        var idDoBotao = this.id; // Obtém o ID do botão clicado
-          
-        switch (idDoBotao){ // Executa ação com base no ID do botão
-          case 'op-a': //Caso clique no botão da opção A            
-            opA.style.backgroundColor = "green";
-            opB.style.backgroundColor = "blue";
-            opC.style.backgroundColor = "blue";
-            statusOpA = "marcado";
-            statusOpB = "desmarcado";
-            statusOpC = "desmarcado";
-            console.log("Teste a -> " + statusOpA);
-            console.log("Teste b -> " + statusOpB); 
-            console.log("Teste c -> " + statusOpC); 
-            break;
-          case 'op-b': //Caso clique no botão da opção B
-            opA.style.backgroundColor = "blue";
-            opB.style.backgroundColor = "green";
-            opC.style.backgroundColor = "blue";
-            statusOpA = "desmarcado";
-            statusOpB = "marcado";
-            statusOpC = "desmarcado";
-            console.log("Teste a -> " + statusOpA);
-            console.log("Teste b -> " + statusOpB); 
-            console.log("Teste c -> " + statusOpC); 
-            break;
-          case 'op-c': //Caso clique no botão da opção C
-            opA.style.backgroundColor = "blue";
-            opB.style.backgroundColor = "blue";
-            opC.style.backgroundColor = "green";
-            statusOpA = "desmarcado";
-            statusOpB = "desmarcado";
-            statusOpC = "marcado";
-            console.log("Teste a -> " + statusOpA);
-            console.log("Teste b -> " + statusOpB); 
-            console.log("Teste c -> " + statusOpC); 
-            break;
-          default:
-            console.log("Botão não reconhecido");
-        }
-      });
-    });
-  //}
+// Adiciona um evento de clique a todos os elementos com a classe "op"
+document.querySelectorAll('.op').forEach(function(botao){
+  botao.addEventListener('click', function(){        
+    var idDoBotao = this.id; // Obtém o ID do botão clicado
+      
+    switch (idDoBotao){ // Executa ação com base no ID do botão
+      case 'op-a': //Caso clique no botão da opção A            
+        opA.style.backgroundColor = "green";
+        opB.style.backgroundColor = "blue";
+        opC.style.backgroundColor = "blue";
+        statusOpA = "marcado";
+        statusOpB = "desmarcado";
+        statusOpC = "desmarcado";
+        console.log("Teste a -> " + statusOpA);
+        console.log("Teste b -> " + statusOpB); 
+        console.log("Teste c -> " + statusOpC); 
+        break;
+      case 'op-b': //Caso clique no botão da opção B
+        opA.style.backgroundColor = "blue";
+        opB.style.backgroundColor = "green";
+        opC.style.backgroundColor = "blue";
+        statusOpA = "desmarcado";
+        statusOpB = "marcado";
+        statusOpC = "desmarcado";
+        console.log("Teste a -> " + statusOpA);
+        console.log("Teste b -> " + statusOpB); 
+        console.log("Teste c -> " + statusOpC); 
+        break;
+      case 'op-c': //Caso clique no botão da opção C
+        opA.style.backgroundColor = "blue";
+        opB.style.backgroundColor = "blue";
+        opC.style.backgroundColor = "green";
+        statusOpA = "desmarcado";
+        statusOpB = "desmarcado";
+        statusOpC = "marcado";
+        console.log("Teste a -> " + statusOpA);
+        console.log("Teste b -> " + statusOpB); 
+        console.log("Teste c -> " + statusOpC); 
+        break;
+      default:
+        console.log("Botão não reconhecido");
+    }
+  });
+});
 
 // ###################### BOTÃO DE RESPOSTA ######################
 //1 - Iniciar quiz sem a imagem, resposta e mensagem de acerto;
